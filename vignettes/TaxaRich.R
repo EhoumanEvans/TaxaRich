0a399941dee441de0abaa423e52bb9cc3c4554c7
Ehouman Evans <36210209+EhoumanEvans@users.noreply.github.com>
2019-04-21 21:33:10 +0000

    upload vignettes files
    these files are automatically generated when package is checked

---
+++ vignettes/TaxaRich.R
@@ -0,0 +1,9 @@
+## ---- echo = FALSE, message = FALSE--------------------------------------
+knitr::opts_chunk$set(collapse = T, comment = "#>")
+options(tibble.print_min = 4L, tibble.print_max = 4L)
+library(dplyr)
+set.seed(1014)
+
+## ----setup---------------------------------------------------------------
+library(TaxaRich)
+
